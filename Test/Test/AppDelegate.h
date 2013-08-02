d14889b0de4a13d49a1dda67f4aac6752e564dc0
Charanjit Singh Bhalla <bhallacharanjit@gmail.com>
2013-08-02 10:13:34 +0530

    Some changes done

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Charanjit Singh Bhalla on 02/08/13.
+//  Copyright (c) 2013 VP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Charanjit Singh Bhalla on 02/08/13.
+//  Copyright (c) 2013 VP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
